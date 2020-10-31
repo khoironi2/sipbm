fe2f5c19f703f11ffae079483d90f913812d277a
khoironi2 <khoironi7887@students.amikom.ac.id>
2020-10-31 11:02:20 +0700

    CRUD DATA USERS done=read,detail,update,delete. belum=update password

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -30,7 +30,7 @@ class Admin extends CI_Controller
     // |------------------------------------------------------
     // | Data Master
     // |------------------------------------------------------
-    // users
+    // data users
     public function users()
     {
         $data = [
@@ -38,13 +38,123 @@ class Admin extends CI_Controller
         ];
         $data['users'] = $this->db->get_where('tbl_users', ['email' =>
         $this->session->userdata('email')])->row_array();
+        $data['allusers'] = $this->Users_model->getAllUsers();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar');
         $this->load->view('templates/topbar');
         $this->load->view('admin/users/index');
         $this->load->view('templates/footer');
     }
+    public function registerFormUsers()
+    {
 
+        $this->form_validation->set_rules('name', 'Nama', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|is_unique[tbl_users.email]|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+        $this->form_validation->set_rules('confrim_password', 'Konfirmasi Password', 'required|trim|matches[password]');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $errors = $this->form_validation->error_array();
+            $this->session->set_flashdata('errors', $errors);
+            $this->session->set_flashdata('input', $this->input->post());
+            redirect('Admin/users');
+        } else {
+
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            $pass = password_hash($password, PASSWORD_DEFAULT);
+            $level = $this->input->post('level');
+            date_default_timezone_set("ASIA/JAKARTA");
+            $data = [
+                'name' => $name,
+                'email' => $email,
+                'password' => $pass,
+                'level' => $level,
+                'time_create_users' => date('Y-m-d H:i:s')
+            ];
+
+            $insert = $this->Auth_model->register("tbl_users", $data);
+            //$insert = $this->db->insert('tbl_users', $data);
+
+            if ($insert) {
+
+                $this->session->set_flashdata('success', '<div class="alert alert-success" role="alert">Sukses, Data user berhasil ditambahkan !</div>');
+                redirect('Admin/users');
+            }
+        }
+    }
+
+    public function updateUsers()
+    {
+        $this->form_validation->set_rules('name', 'name', 'required');
+        $this->form_validation->set_rules('email', 'email', 'required');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+        if ($this->form_validation->run() == FALSE) {
+            $errors = $this->form_validation->error_array();
+            $this->session->set_flashdata('errors', $errors);
+            $this->session->set_flashdata('input', $this->input->post());
+            redirect('Admin/users');
+        } else {
+            $name = $this->input->post('name');
+            $email = $this->input->post('email');
+            $id = $this->input->post('id_users');
+            $level = $this->input->post('level');
+            date_default_timezone_set("ASIA/JAKARTA");
+            $data = [
+                'name' => $name,
+                'email' => $email,
+                'level' => $level,
+                'time_update_users' => date('Y-m-d H:i:s')
+            ];
+            $update = $this->Users_model->update($id, $data);
+            if ($update) {
+                $this->session->set_flashdata('success', '<div class="alert alert-success" role="alert">Sukses, Data berhasil di update !</div>');
+                redirect('Admin/users');
+            }
+        }
+    }
+
+    public function updatePwdUsers()
+    {
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+        $this->form_validation->set_rules('id_users', 'Id Users', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $errors = $this->form_validation->error_array();
+            $this->session->set_flashdata('errors', $errors);
+            $this->session->set_flashdata('input', $this->input->post());
+            redirect('Admin/');
+        } else {
+            $password = $this->input->post('password');
+            $pass = password_hash($password, PASSWORD_DEFAULT);
+            $id = $this->input->post('id_users');
+
+            // date_default_timezone_set("ASIA/JAKARTA");
+            $data = [
+                'password' => $pass
+                // 'time_update_users' => date('Y-m-d H:i:s')
+            ];
+            $update = $this->Users_model->update($id, $data);
+            if ($update) {
+                $this->session->set_flashdata('success', '<div class="alert alert-success" role="alert">Sukses, Data berhasil di update !</div>');
+                redirect('Admin/users');
+            }
+        }
+    }
+
+    public function deleteusers($id)
+    {
+        $data['id_users'] = $this->Users_model->delete($id);
+        $data['id_users'] = $this->Users_model->delete($id);
+        $this->session->set_flashdata('success', '<div class="alert alert-success" role="alert">Sukses, Data berhasil di Hapus!</div>');
+        redirect('Admin/users');
+    }
+
+
+    //end data users
     public function update_users()
     {
         $data = [
@@ -63,6 +173,8 @@ class Admin extends CI_Controller
         $data = [
             'title' => 'Admin | Koleksi'
         ];
+        $data['users'] = $this->db->get_where('tbl_users', ['email' =>
+        $this->session->userdata('email')])->row_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar');
         $this->load->view('templates/topbar');
--- application/views/admin/users/index.php
+++ application/views/admin/users/index.php
@@ -3,6 +3,7 @@
     <i class="fas fa-plus"></i>
     Tambah Data User
 </button>
+<p><?php echo $this->session->flashdata('success'); ?></p>
 
 <div class="card shadow mb-4">
     <div class="card-header py-3">
@@ -16,7 +17,7 @@
                         <th>No</th>
                         <th>Id User</th>
                         <th>Nama</th>
-                        <th>Password</th>
+                        <!-- <th>Password</th> -->
                         <th>Level</th>
                         <th>Aksi</th>
                     </tr>
@@ -26,70 +27,252 @@
                         <th>No</th>
                         <th>Id User</th>
                         <th>Nama</th>
-                        <th>Password</th>
+                        <!-- <th>Password</th> -->
                         <th>Level</th>
                         <th>Aksi</th>
                     </tr>
                 </tfoot>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>US001</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>2011/04/25</td>
-                        <td>
-                            <a href="<?= base_url('admin/update_users') ?>" class="btn btn-info btn-sm"><i class="fas fa-edit"></i></a>
-                            <a href="" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
-                        </td>
-                    </tr>
+                    <?php $no = 1;
+                    foreach ($allusers as $datausers) : ?>
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $datausers->id_users; ?></td>
+                            <td><?= $datausers->name; ?></td>
+                            <!-- <td><?= $datausers->password; ?></td> -->
+                            <td><?= $datausers->level; ?></td>
+                            <td>
+                                <a href="" data-toggle="modal" data-target="#ModalEdit<?= $datausers->id_users; ?>" class="btn btn-success btn-sm"><i class="fas fa-edit"></i></a>
+                                <a href="" data-toggle="modal" data-target="#ModalDetail<?= $datausers->id_users; ?>" class="btn btn-info btn-sm"><i class="fas fa-info-circle"></i></a>
+                                <a href="" data-toggle="modal" data-target="#ModalUpdatePwd<?= $datausers->id_users; ?>" class="btn btn-warning btn-sm"><i class="fas fa-key"></i></a>
+                                <a href="" data-toggle="modal" data-target="#ModalHapus<?= $datausers->id_users; ?>" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
 
-<!-- modal -->
+<!-- modal tambah users -->
 <div class="modal fade" id="formDataUser" tabindex="-1" aria-labelledby="formDataUserLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="formDataUserLabel">Form Tambah Data User</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <form action="" method="POST">
-                <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" class="form-control" id="nama" placeholder="Masukan nama user">
-                </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="formDataUserLabel">Form Tambah Data User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= base_url('Admin/registerFormUsers') ?>" method="POST">
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" name="name" class="form-control" id="nama" placeholder="Masukan nama user">
+                    </div>
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Masukan email user">
-                </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" placeholder="Masukan email user">
+                    </div>
 
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Masukan password user">
-                </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Masukan password user">
+                        <div style="margin-top:10px"></div> <input name="confrim_password" type="password" id="inputPassword" class="form-control" placeholder="Confrim Password" required>
+                    </div>
 
-                <div class="form-group">
-                    <label for="level">Level</label>
-                    <select class="form-control" id="level">
-                        <option value="admin">Admin</option>
-                        <option value="pihak_pusat">Pihak Pusat</option>
-                        <option value="pengelola">Pengelola</option>
-                        <option value="petugas_perawatan">Petugas Perawatan</option>
-                    </select>
-                </div>
+                    <div class="form-group">
+                        <label for="level">Level</label>
+                        <select name="level" class="form-control" id="level">
+                            <option value="admin">Admin</option>
+                            <option value="pihak_pusat">Pihak Pusat</option>
+                            <option value="pengelola">Pengelola</option>
+                            <option value="petugas_perawatan">Petugas Perawatan</option>
+                        </select>
+                    </div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Tambah</button>
+            </div>
             </form>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary">Tambah</button>
+    </div>
+</div>
+<!-- end modal tambah users -->
+
+<!-- Modal Detail users -->
+<?php foreach ($allusers as $datausers) : ?>
+    <div class="modal fade" id="ModalDetail<?= $datausers->id_users; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Detail Users <?= $datausers->name; ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="POST">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" name="name" value="<?= $datausers->name; ?>" class="form-control" id="nama" placeholder="Masukan nama user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" value="<?= $datausers->email; ?>" class="form-control" id="email" placeholder="Masukan email user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="text" name="password" value="<?= $datausers->password; ?>" class="form-control" id="password" placeholder="Masukan password user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="level">Level</label>
+                            <select name="level" class="form-control" id="level">
+                                <option value="<?= $datausers->level; ?>"><?= $datausers->level; ?></option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Waktu Dibuat</label>
+                            <input type="text" value="<?= $datausers->time_create_users; ?>" class="form-control" id="password" placeholder="Masukan password user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Terakhir Login</label>
+                            <input type="text" value="<?= $datausers->time_login_users; ?>" class="form-control" id="password" placeholder="Masukan password user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Terakhir Logout</label>
+                            <input type="text" value="<?= $datausers->time_logout_users; ?>" class="form-control" id="password" placeholder="Masukan password user">
+                        </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+                </form>
+            </div>
         </div>
     </div>
-  </div>
+    </div>
+<?php endforeach ?>
+<!-- end Modal Detail Users -->
+
+<!-- Modal Edit users -->
+<?php foreach ($allusers as $datausers) : ?>
+    <div class="modal fade" id="ModalEdit<?= $datausers->id_users; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Update Users <?= $datausers->name; ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="<?= base_url('Admin/updateUsers') ?>" method="POST">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" name="name" value="<?= $datausers->name; ?>" class="form-control" id="nama" placeholder="Masukan nama user">
+                            <input type="text" hidden name="id_users" value="<?= $datausers->id_users; ?>" class="form-control" id="nama" placeholder="Masukan nama user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" value="<?= $datausers->email; ?>" class="form-control" id="email" placeholder="Masukan email user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="level">Level</label>
+                            <select name="level" class="form-control" id="level">
+                                <option value="<?= $datausers->level ?>"><?= $datausers->level ?></option>
+                                <option value="admin">Admin</option>
+                                <option value="pihak_pusat">Pihak Pusat</option>
+                                <option value="pengelola">Pengelola</option>
+                                <option value="petugas_perawatan">Petugas Perawatan</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">UPDATE</button>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">BATALKAN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach ?>
+<!-- end Modal Edit Users -->
+
+<!-- Modal Update password users -->
+<?php foreach ($allusers as $datausers) : ?>
+    <div class="modal fade" id="ModalUpdatePwd<?= $datausers->id_users; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Update Passowrd <?= $datausers->name; ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="<?= base_url('Admin/updatePwdUsers') ?>" method="POST">
+
+                        <div class="form-group">
+                            <input type="text" disabled hidden name="id_users" value="<?= $datausers->id_users; ?>" class="form-control" id="id_users" placeholder="Masukan nama user">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" placeholder="Masukan password user" required>
+
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success">UPDATE</button>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">BATALKAN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach ?>
+<!-- end Modal Update Password Users -->
+
+
+<?php foreach ($allusers as $datausers) : ?>
+    <div class="modal fade" id="ModalHapus<?= $datausers->id_users; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Hapus Users <?= $datausers->name; ?></h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="alert alert-danger" role="alert">
+                        <h4 class="alert-heading">Anda yakin ?</h4>
+                        <p>Jika anda menghapus users <b><?= $datausers->name; ?></b> maka data dari user tersebut terhapus dari sistem !!. Jika ada kesalahan dan perlu di ubah maka lakukan update data dengan mengklik tombol EDIT yang berwarna hijau !!</p>
+                        <hr>
+                        <p class="mb-0">Namun jika memang anda sudah yakin maka silahkan klik tombol Yakin !</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-warning" data-dismiss="modal">Tutup</button>
+                        <a class="btn btn-danger" href="<?= base_url('Admin/deleteusers/' . $datausers->id_users) ?>">YAKIN !</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endforeach ?>
