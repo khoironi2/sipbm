d24056c6a9eae50ae00f37389f2c273b49be5c71
Abdul Rosyid <abdulrosyid@localhost.localdomain>
2020-10-31 23:06:33 -0500

    Update Profile

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -936,10 +936,50 @@ class Admin extends CI_Controller
             'title' => 'Admin | Profile',
             'users' => $this->db->get_where('tbl_users', ['email' => $this->session->userdata('email')])->row_array(),
         ];
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar');
-        $this->load->view('templates/topbar');
-        $this->load->view('admin/profile/index');
-        $this->load->view('templates/footer');
+        $old_image = $data['users']['gambar_users'];
+
+        $this->form_validation->set_rules('name', 'nama', 'required');
+        $this->form_validation->set_rules('telepon_users', 'telepon', 'required');
+        $this->form_validation->set_rules('alamat_users', 'telepon', 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar');
+            $this->load->view('templates/topbar');
+            $this->load->view('admin/profile/index');
+            $this->load->view('templates/footer');
+        } else {
+            $data = [
+                'name' => $this->input->post('name'),
+                'telepon_users' => $this->input->post('telepon_users'),
+                'alamat_users' => $this->input->post('alamat_users')
+            ];
+
+            $upload_image = $_FILES['gambar_users']['name'];
+
+            if ($upload_image) {
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']      = '2048';
+                $config['upload_path'] = './assets/img/users/';
+
+                $this->load->library('upload', $config);
+
+                if ($this->upload->do_upload('gambar_users')) {
+                    if ($old_image != 'default.png') {
+                        unlink(FCPATH . 'assets/img/users/' . $old_image);
+                    }
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('gambar_users', $new_image);
+                } else {
+                    echo $this->upload->display_errors();
+                }
+            }
+
+            $this->db->where('email', $this->input->post('email'));
+            $this->db->update('tbl_users', $data);
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Data Berhasil Diubah!</div>');
+            redirect('admin/profile');
+        }
     }
 }
--- application/views/admin/profile/index.php
+++ application/views/admin/profile/index.php
@@ -1,28 +1,40 @@
 <div class="row">
     <div class="col-sm-7"> 
+        <?= $this->session->flashdata('message'); ?>
         <div class="card">
             <div class="card-header">
                 Edit Profile Admin
             </div>
             <div class="card-body">
-                <form action="" method="POST">
+                <form action="<?= base_url('admin/profile') ?>" method="POST" enctype="multipart/form-data">
                     <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" class="form-control" id="nama" name="nama">
+                        <label for="name">Nama</label>
+                        <input type="text" class="form-control" id="name" name="name" value="<?= $users["name"]; ?>">
+                        <?= form_error('name', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
-                        <label for="telepon">Telepon</label>
-                        <input type="text" class="form-control" id="telepon" name="telepon">
+                        <label for="email">Email</label>
+                        <input type="text" class="form-control" id="email" name="email" value="<?= $users["email"]; ?>" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="telepon_users">Telepon Users</label>
+                        <input type="text" class="form-control" id="telepon_users" name="telepon_users" value="<?= $users["telepon_users"]; ?>">
+                        <?= form_error('telepon_users', '<small class="text-danger pl-3">', '</small>'); ?>
+                    </div>
+                    <div class="form-group">
+                        <label for="alamat_users">Alamat</label>
+                        <textarea type="text" class="form-control" id="alamat_users" name="alamat_users"><?= $users["alamat_users"]; ?></textarea>
+                        <?= form_error('alamat_users', '<small class="text-danger pl-3">', '</small>'); ?>
                     </div>
                     <div class="form-group">
                         <div class="row">
                             <div class="col-sm-3">
-                                <img src="<?= base_url('assets/img/Logo.png') ?>" alt="" class="img-thumbnail">
+                                <img src="<?= base_url('assets/img/users/' . $users["gambar_users"]); ?>" alt="" class="img-thumbnail">
                             </div>
                             <div class="col-sm-9">
-                                <label for="image">Image</label>
+                                <label for="gambar_users">Image</label>
                                 <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="image">
+                                    <input type="file" class="custom-file-input" id="gambar_users" name="gambar_users">
                                     <label class="custom-file-label" for="image">Choose file</label>
                                 </div>
                             </div>
@@ -30,7 +42,7 @@
                     </div>
 
                     <div class="form-group">
-                        <button class="btn btn-primary mt-3">Update</button>
+                        <button type="submit" class="btn btn-primary mt-3">Update</button>
                     </div>
                 </form>
             </div>
