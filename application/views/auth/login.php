71ab8b801ec925535537320fd7a4a1d296c1b5f0
abdulrosyidit <abdulrosyidit@gmail.com>
2020-11-02 09:15:20 +0700

    Mockup Login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,55 +1,26 @@
 <div class="container">
-
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
         <div class="col-lg-6">
 
             <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="alert alert-dark text-center">
+                    Sistem Informasi Perawatan Benda Museum
+                </div>
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
 
                         <div class="col-lg">
                             <div class="p-5">
-                                <div class="text-center">
+                                <!-- <div class="text-center">
                                     <h2><img src="<?= base_url('assets/img/Logo.png') ?>" height="70" alt=""></h2>
                                     <br>
-                                </div>
+                                </div> -->
 
                                 <form class="user" method="post" action="<?= base_url('Auth/loginForm') ?>">
-                                    <?php
-                                    $errors = $this->session->flashdata('errors');
-                                    if (!empty($errors)) {
-                                    ?>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="alert alert-danger text-center">
-                                                    <?php foreach ($errors as $key => $error) { ?>
-                                                        <?php echo "$error<br>"; ?>
-                                                    <?php } ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php
-                                    }
-                                    if ($msg = $this->session->flashdata('error_login')) { ?>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="alert alert-danger text-center">
-                                                    <?= $msg ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php } else if ($msg = $this->session->flashdata('success_login')) { ?>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="alert alert-success text-center">
-                                                    <?= $msg ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php } ?>
+
                                     <div class="form-group">
                                         <input class="form-control form-control-user" name="email" type="email" id="inputEmail" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
 
@@ -69,13 +40,53 @@
                                     <a href="#" data-toggle="modal" data-target="#exampleModal">
                                         Register </a>
                                 </div>
-
+                            </div>
+                        </div>
+                    </div>
+                    <div class="alert alert-dark mb-0">
+                        <div class="row">
+                            <div class="col-sm">
+                                <?= date('H:i:s'); ?>
+                            </div>
+                            <div class="col-sm text-right">
+                                <?= date('d-M-Y') ?>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
+            <?php
+            $errors = $this->session->flashdata('errors');
+            if (!empty($errors)) {
+            ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-danger text-center">
+                            <?php foreach ($errors as $key => $error) { ?>
+                                <?php echo "$error<br>"; ?>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            if ($msg = $this->session->flashdata('error_login')) { ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-danger text-center">
+                            <?= $msg ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } else if ($msg = $this->session->flashdata('success_login')) { ?>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="alert alert-success text-center">
+                            <?= $msg ?>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
         </div>
 
     </div>
